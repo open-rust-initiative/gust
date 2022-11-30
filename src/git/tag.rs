c5764ebbf9cd9cff3e43a7502cab4056e6923526
Luxian <xiaoyang@isrc.iscas.ac.cn>
2022-11-30 14:42:06 +0800

    [fixbug] : fix a offset file bug of the Tag parse

--- src/git/tag.rs
+++ src/git/tag.rs
@@ -6,16 +6,16 @@
 //!
 //!
 
-use bstr::{ByteSlice, BString};
+use bstr:: ByteSlice;
 
 use crate::errors::GitError;
 
-use crate::git::id::ID;
-use crate::git::Metadata;
 use crate::git::hash::HashType;
-use crate::git::sign::AuthorSign;
+use crate::git::id::ID;
 use crate::git::object::types::ObjectType;
-use std::fmt::Display;
+use crate::git::sign::AuthorSign;
+use crate::git::Metadata;
+use std::fmt::{Display, write};
 /// Git Object: tag
 #[allow(unused)]
 #[derive(PartialEq, Eq, Debug, Hash, Ord, PartialOrd, Clone)]
@@ -30,24 +30,26 @@ pub struct Tag {
 
 ///
 impl Tag {
-    pub fn new(meta:Metadata)->Self {
-        let mut  a = Self{
-            meta:meta.clone(),
+    /// Tag 的构造函数 接收一个@param meta::Metadata
+    /// 同时执行tag解码
+    pub fn new(meta: Metadata) -> Self {
+        
+        let mut a = Self {
+            meta: meta.clone(),
             object: meta.id.clone(),
             t: ObjectType::Commit,
-            tag: "v1.1.0".to_string(),
-            tagger:AuthorSign {
+            tag: "".to_string(),
+            tagger: AuthorSign {
                 t: "".to_string(),
                 name: "".to_string(),
                 email: "".to_string(),
                 timestamp: 0,
-                timezone: "+0000".to_string()
+                timezone: "".to_string(),
             },
-            message: "".to_string(),  
+            message: "".to_string(),
         };
-        a.decode_metadata();
+        a.decode_metadata().unwrap();
         a
-        
     }
 
     ///
@@ -67,15 +69,25 @@ impl Tag {
 
         let tag_begin = data.find_byte(0x20).unwrap();
         let tag_end = data.find_byte(0x0a).unwrap();
-        self.tag = data[tag_begin + 1..tag_end].to_str().unwrap().parse().unwrap();
-        data = data[type_end..].to_vec();
+        self.tag = data[tag_begin + 1..tag_end]
+            .to_str()
+            .unwrap()
+            .parse()
+            .unwrap();
+        data = data[tag_end + 1 ..].to_vec();//Fixed Bug: bug tyep_end to tag_end 
+       
 
-        let tagger = data.find("tagger").unwrap();
-        let tagger_data = data[.. data.find_byte(0x0a).unwrap()].to_vec();
+        let tagger_begin = data.find("tagger").unwrap();
+        let tagger_end = data.find_byte(0x0a).unwrap();
+        let tagger_data = data[tagger_begin..tagger_end].to_vec();
         self.tagger.decode_from_data(tagger_data)?;
         data = data[data.find_byte(0x0a).unwrap() + 1..].to_vec();
 
-        self.message = data[data.find_byte(0x0a).unwrap()..].to_vec().to_str().unwrap().to_string();
+        self.message = data[data.find_byte(0x0a).unwrap()..]
+            .to_vec()
+            .to_str()
+            .unwrap()
+            .to_string();
 
         Ok(())
     }
@@ -104,14 +116,13 @@ impl Tag {
         data.extend_from_slice(0x0au8.to_be_bytes().as_ref());
         data.extend_from_slice(self.message.as_bytes());
 
-        Ok(
-            Metadata {
-                t: ObjectType::Tag,
-                h: HashType::Sha1,
-                id: ID::from_vec(ObjectType::Tag, &mut data),
-                size: data.len(),
-                data,
-            })
+        Ok(Metadata {
+            t: ObjectType::Tag,
+            h: HashType::Sha1,
+            id: ID::from_vec(ObjectType::Tag, &mut data),
+            size: data.len(),
+            data,
+        })
     }
 
     ///
@@ -120,11 +131,13 @@ impl Tag {
         self.meta.write_to_file(root_path)
     }
 }
-impl Display for Tag{
+impl Display for Tag {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        writeln!(f,"Type: Tag");
-        writeln!(f, "{}", BString::new(self.meta.data.clone()))
-    }   
+        writeln!(f, "Type: Tag");
+        writeln!(f, "Tag: {}", self.tag);
+        self.tagger.fmt(f);
+        writeln!(f,"{}",self.message)
+    }
 }
 ///
 #[cfg(test)]
@@ -132,12 +145,13 @@ mod tests {
     use std::env;
     use std::path::Path;
     use std::path::PathBuf;
+    use std::str::FromStr;
 
-    use crate::git::Metadata;
-    use crate::git::ObjectType;
     use crate::git::hash::Hash;
     use crate::git::id::ID;
     use crate::git::sign::AuthorSign;
+    use crate::git::Metadata;
+    use crate::git::ObjectType;
 
     use super::Tag;
 
@@ -152,11 +166,17 @@ mod tests {
 
         assert_eq!(ObjectType::Tag, meta.t);
         assert_eq!(976, meta.size);
-        assert_eq!("e5c324b03b72b26f11557c4955c6d17c68dc8595", meta.id.to_string());
+        assert_eq!(
+            "e5c324b03b72b26f11557c4955c6d17c68dc8595",
+            meta.id.to_string()
+        );
 
         let mut tag = Tag {
             meta,
-            object: ID { bytes: vec![], hash: "".to_string() },
+            object: ID {
+                bytes: vec![],
+                hash: "".to_string(),
+            },
             t: ObjectType::Commit,
             tag: "".to_string(),
             tagger: AuthorSign {
@@ -164,19 +184,48 @@ mod tests {
                 name: "".to_string(),
                 email: "".to_string(),
                 timestamp: 0,
-                timezone: "+0000".to_string()
+                timezone: "+0000".to_string(),
             },
             message: "".to_string(),
         };
 
         tag.decode_metadata().unwrap();
-
-        assert_eq!("6414e45babf0bdd043ba40d31123053cfebef26c", tag.object.to_string());
+        
+        assert_eq!(
+            "6414e45babf0bdd043ba40d31123053cfebef26c",
+            tag.object.to_string()
+        );
         assert_eq!("commit", tag.t.to_string());
         assert_eq!("v1.1.0", tag.tag);
         assert_eq!(1653037847, tag.tagger.timestamp);
+        println!("{}",tag);
     }
 
+    #[test]
+    fn test_output_meat() {
+        let meta = Metadata {
+            t: ObjectType::Tag,
+            h: crate::git::HashType::Sha1,
+            id: ID::from_string("df1087c478c8d337cb587b897e86f2455e2687ed"),
+            size: 155,
+            data: vec![
+                111, 98, 106, 101, 99, 116, 32, 51, 55, 50, 49, 51, 101, 55, 98, 98, 51, 99, 51,
+                51, 52, 97, 48, 102, 55, 55, 48, 56, 99, 55, 97, 102, 99, 97, 98, 53, 98, 97, 98,
+                98, 51, 102, 57, 53, 52, 51, 52, 10, 116, 121, 112, 101, 32, 99, 111, 109, 109,
+                105, 116, 10, 116, 97, 103, 32, 48, 46, 49, 10, 116, 97, 103, 103, 101, 114, 32,
+                97, 100, 105, 116, 121, 97, 32, 60, 100, 101, 118, 64, 99, 104, 105, 109, 101, 114,
+                97, 99, 111, 100, 101, 114, 46, 110, 101, 116, 62, 32, 49, 52, 50, 56, 54, 49, 50,
+                48, 48, 55, 32, 45, 48, 52, 48, 48, 10, 10, 70, 105, 114, 115, 116, 32, 105, 109,
+                112, 108, 101, 109, 101, 110, 116, 97, 116, 105, 111, 110, 32, 111, 102, 32, 116,
+                104, 101, 32, 99, 108, 105, 10,
+            ],
+        };
+
+        let tag = Tag::new(meta);
+        
+        
+        println!("{}", tag);
+    }
     ///
     #[test]
     fn test_tag_write_to_file() {
@@ -185,7 +234,10 @@ mod tests {
             t: ObjectType::Tag,
             h: HashType::Sha1,
             size: 0,
-            id: super::ID { bytes: vec![], hash: "".to_string() },
+            id: super::ID {
+                bytes: vec![],
+                hash: "".to_string(),
+            },
             data: vec![],
         };
 
@@ -194,7 +246,7 @@ mod tests {
             name: "Quanyi Ma".to_string(),
             email: "eli@patch.sh".to_string(),
             timestamp: 1653037847,
-            timezone: "+0800".to_string()
+            timezone: "+0800".to_string(),
         };
 
         let mut tag = Tag {
@@ -207,10 +259,12 @@ mod tests {
         };
 
         tag.meta = tag.encode_metadata().unwrap();
-        assert_eq!("e5c324b03b72b26f11557c4955c6d17c68dc8595", tag.meta.id.to_string());
+        assert_eq!(
+            "e5c324b03b72b26f11557c4955c6d17c68dc8595",
+            tag.meta.id.to_string()
+        );
 
         tag.write_to_file("/tmp".to_string()).expect("Write error!");
         assert!(Path::new("/tmp/e5/c324b03b72b26f11557c4955c6d17c68dc8595").exists());
-
     }
 }
